0b95c2838a27e2d55a14b689f96043c4b44d9492
AkshayBorana <akshayborana1@gmail.com>
2025-06-15 23:02:14 -0400

    feat: TECH-2 Added insights dashboard component and added its route.

---
+++ src/features/insights/insights.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insights',
+  templateUrl: './insights.component.html',
+  styleUrl: './insights.component.scss',
+  imports: [],
+})
+
+export class InsightsComponent {
+
+  constructor() {}
+
+}
