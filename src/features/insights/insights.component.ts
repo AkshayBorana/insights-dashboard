360be1611de6fbfd84c9f692eedfd2df6f4575b7
AkshayBorana <akshayborana1@gmail.com>
2025-06-15 23:14:20 -0400

    feat: TECH-3 Built reusable shared chart component.

--- src/features/insights/insights.component.ts
+++ src/features/insights/insights.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
+import { ChartComponent } from '../../shared/components/chart/chart.component';
 
 @Component({
   selector: 'app-insights',
   templateUrl: './insights.component.html',
   styleUrl: './insights.component.scss',
-  imports: [],
+  imports: [ChartComponent],
 })
 
 export class InsightsComponent {
-
   constructor() {}
-
 }
---
+++ src/shared/components/chart/chart.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrl: './chart.component.scss',
+  imports: []
+})
+
+
+export class ChartComponent {
+
+  constructor() {}
+
+}
